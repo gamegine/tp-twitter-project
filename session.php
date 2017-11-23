800369b85df1a8daf2337217a0fc774eb2b25e99
gamgine <victor.fromentin95@ynov.com>
2017-11-23 22:21:00 +0100

    Create session.php

---
+++ session.php
@@ -0,0 +1,10 @@
+<?php session_start(); ?>
+<!doctype html>
+<html lang="fr">
+  <?php include("./asset/head.php"); ?>
+<body>
+	<?php include("./asset/header.php"); ?>
+  <main></main>
+  <?php include("./asset/footer.php"); ?>
+</body>
+</html>
