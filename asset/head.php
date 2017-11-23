fef78444b9123f3c687b7eb8b3fedf7c545ec58b
gamgine <victor.fromentin95@ynov.com>
2017-11-23 22:17:25 +0100

    Create head.php

---
+++ asset/head.php
@@ -0,0 +1,6 @@
+<head>
+	<meta charset="UTF-8">
+	<title><?php echo (!empty($title))?$title.' || twitter-project':'twitter-project'; ?></title>
+	<meta name="LANGUAGE" content="FR">
+	<meta name="Content-Language" content="fr">
+</head>
