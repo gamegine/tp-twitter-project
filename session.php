1604e309bf629d1464dee58dc1e96e6cdad222ad
gamgine <victor.fromentin95@ynov.com>
2017-11-23 23:07:16 +0100

    create form login/register

--- session.php
+++ session.php
@@ -3,8 +3,30 @@
 <html lang="fr">
   <?php include("./asset/head.php"); ?>
 <body>
-	<?php include("./asset/header.php"); ?>
-  <main></main>
+  <?php include("./asset/header.php"); ?>
+  <main>
+    <?php echo (!empty($_SESSION['msg']))? "<div>". $_SESSION['msg']."</div>":''; ?>
+    <form>
+      <legend>login</legend>
+      <label for="uname">user name:</label><br>
+      <input type="text" name="uname" <?php echo (!empty($_SESSION['formname']))? 'value="'.$_SESSION['formname'].'"':''; ?>>
+      <br>
+      <label for="uname">password:</label><br>
+      <input type="password" name="passw">
+      <input type="hidden" name="a" value="l">
+      <input type="submit" value="Submit">
+    </form>
+    <form>
+      <legend>register</legend>
+      <label for="uname">user name:</label><br>
+      <input type="text" name="uname" <?php echo (!empty($_SESSION['formname']))? 'value="'.$_SESSION['formname'].'"':''; ?>>
+      <br>
+      <label for="uname">password:</label><br>
+      <input type="password" name="passw">
+      <input type="hidden" name="a" value="r">
+      <input type="submit" value="Submit">
+    </form>
+  </main>
   <?php include("./asset/footer.php"); ?>
 </body>
 </html>
