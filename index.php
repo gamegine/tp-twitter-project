1cd10ee7f98c63bef4dc5a3e7792e663eb064a9e
gamgine <victor.fromentin95@ynov.com>
2017-11-23 23:27:43 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,10 +5,10 @@ include("asset/class.php");
 ?>
 <!doctype html>
 <html lang="fr">
-  <?php include("./asset/head.php"); ?>
-<body>
-	<?php include("./asset/header.php"); ?>
-  <main></main>
-  <?php include("./asset/footer.php"); ?>
-</body>
+	<?php include("./asset/head.php"); ?>
+	<body>
+		<?php include("./asset/header.php"); ?>
+		<main></main>
+		<?php include("./asset/footer.php"); ?>
+	</body>
 </html>
