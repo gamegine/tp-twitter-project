354798d261ae43bd4c2c0153504cca14468ca887
gamgine <victor.fromentin95@ynov.com>
2017-11-24 01:55:07 +0100

    Update head.php

--- asset/head.php
+++ asset/head.php
@@ -3,5 +3,5 @@
 	<title><?php echo (!empty($title))?$title.' || twitter-project':'twitter-project'; ?></title>
 	<meta name="LANGUAGE" content="FR">
 	<meta name="Content-Language" content="fr">
-	<link rel="stylesheet" type="text/css" href="mystyle.css">
+	<link rel="stylesheet" type="text/css" href="/asset/style.css">
 </head>
