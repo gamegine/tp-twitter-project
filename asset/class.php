90957cdf5dcb167ac916a6a256d8e10077cf0d78
gamgine <victor.fromentin95@ynov.com>
2017-11-24 01:24:51 +0100

    debug

--- asset/class.php
+++ asset/class.php
@@ -6,15 +6,16 @@ class user
   {
     $this->msg = array();
     global $bdd;
-    $reponse = $bdd->prepare('SELECT `uid`,`name` FROM `user` where uid = :uid');
+    $reponse = $bdd->prepare('SELECT `uid`,`name` FROM `user` where uid = 1');
+	$reponse->bindValue(':uid',$id,PDO::PARAM_INT);
     $reponse->execute();
     $donnees = $reponse->fetch();
-    $this->id=$donnees['id'];
-    $this->id=htmlentities($donnees['name']);
+    $this->id=$donnees['uid'];
+    $this->name=htmlentities($donnees['name']);
     $reponse->closeCursor();
   }
   public function ech()
-  { echo htmlentities($this->name); }
+  { echo '<a>'.htmlentities($this->name).'</a>'; }
 }
 class msg
 {
@@ -39,9 +40,30 @@ class msglist
     $reponse = $bdd->prepare('SELECT * FROM `tw`');
     $reponse->execute();
     while ($donnees = $reponse->fetch())
-    { array_push( $this->msg, new msg( $donnees['id'] , $donnees['uid'] , $donnees['txt'] ) ); }
+    { array_push( $this->msg, new msg( $donnees['id'] , new user($donnees['uid']) , $donnees['txt'] ) ); }
     $reponse->closeCursor();
   }
   public function ech()
   { foreach ($this->msg as &$m) {$m->ech();} }
 }
+/*class msgf
+{
+  public function post($msg)
+  {
+	if(isset($_SESSION['id']))
+	{
+	global $bdd;
+	$reponse = $bdd->prepare('INSERT INTO `tw`(`uid`, `txt`) VALUES (:uid, :txt)');
+	$reponse->bindValue(':uid',$_SESSION['id'],PDO::PARAM_STR);
+	$reponse->bindValue(':txt',htmlentities($msg),PDO::PARAM_STR);
+	$reponse->execute();
+	$reponse->closeCursor();
+    header('Location: /');
+	}
+	else
+	{
+		header('Location: /session.php');
+		$_SESSION['postmsg']=$msg;
+	}
+  }
+}*/
