9b4d6c245a7e25e8793b2dc4f212ecbcbc5846a8
gamgine <victor.fromentin95@ynov.com>
2017-11-30 00:05:44 +0100

    Update db.sql

--- db.sql
+++ db.sql
@@ -3,12 +3,12 @@ CREATE TABLE `twitt` (
   `id` int(11) NOT NULL,
   `uid` int(11) NOT NULL,
   `txt` text NOT NULL
-)
+);
 CREATE TABLE `user` (
   `uid` int(11) NOT NULL,
   `name` varchar(32) NOT NULL,
   `mdp` text NOT NULL
-)
+);
 ALTER TABLE `twitt`ADD PRIMARY KEY (`id`);
 ALTER TABLE `twitt` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 ALTER TABLE `user`ADD PRIMARY KEY (`uid`);
